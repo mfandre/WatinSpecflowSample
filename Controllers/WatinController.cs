1a1a9ec4fa307d27775be37dec78144c7ac91e6d
André de Mattos Ferraz <fandre@gmail.com>
2015-04-13 12:10:37 -0300

    Testing using WatiN and SpecFlow
    This project was a Sample to two arcticles posted for
    
    http://www.c-sharpcorner.com/UploadFile/27c648/automated-ui-test-with-watin-and-specflow-in-net-part-1777/

---
+++ Controllers/WatinController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatinSpecflowSample.Controllers
+{
+    public class WatinController : Controller
+    {
+        //
+        // GET: /Watin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Page1()
+        {
+            return View();
+        }
+
+        public ActionResult Page2()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WatinSpecflowSample.MvcApplication" Language="C#" %>
